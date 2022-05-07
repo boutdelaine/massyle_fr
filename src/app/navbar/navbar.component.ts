3df0bcb7e081b546d76f9d1866e554dbf3b1d7fd
Boutdelaine <chikhimassyle@gmail.com>
2022-05-07 10:57:18 +0200

    Bootstrap installation and Navbar started

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  navItems = [
+    {
+      path :'#portfolio',
+      name : 'Portfolio'
+    },
+    {
+      path :'#about',
+      name : 'About'
+    },
+    {
+      path :'#contact',
+      name : 'Contact'
+    },
+    {
+      path :'/blog',
+      name : 'Blog'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
