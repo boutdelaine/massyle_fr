231ed80a1820676e6c6e5bcbb649e43c5abfe4a6
Boutdelaine <chikhimassyle@gmail.com>
2022-05-07 20:51:54 +0200

    Sections created, footer initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './component/components/footer/footer.component';
+import { WelcomeComponent } from './component/sections/welcome/welcome.component';
+import { PortfolioComponent } from './component/sections/portfolio/portfolio.component';
+import { AboutComponent } from './component/sections/about/about.component';
+import { ContactComponent } from './component/sections/contact/contact.component';
+import { TestimonialsComponent } from './component/sections/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    WelcomeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
